0289c02c7ff6f006a437f34e5fb65c53d8acc9a9
Ashley Darwin <ashleydarwin11@gmail.com>
2023-11-29 07:32:07 -0300

    Main

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,22 @@
     <v-main>
       <router-view />
     </v-main>
-    <v-footer>
+    <!-- <v-footer>
       <AppFooter />
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue';
-import AppFooter from '@/components/AppFooter.vue';
+// import AppFooter from '@/components/AppFooter.vue';
 
 export default {
   name: 'App',
 
   components: {
     TopBar,
-    AppFooter,
+    // AppFooter,
   }, 
 
   data: () => ({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,23 @@
 <template>
   <v-container>
-    <!-- Competencias -->
+    <!-- Competencies -->
     <v-row>
       <v-col>
         <div class="ma-4">
-          <v-img width="580" class=" img rounded-xl" :aspect-ratio="1" src="../assets/competencias-img.png"></v-img>
+          <v-img width="580" class="img rounded-xl" :aspect-ratio="1" src="@/assets/competencies.png"></v-img>
         </div>
       </v-col>
-      <v-col class="ma-2 competencias-content">
+      <v-col class="ma-2">
         <p class="text-h4 mb-8 font-weight-bold">Desenvolvimento e crescimento de competências</p>
-        <p class="desc">Todo o trabalho desenvolvido no Luiza de Teodoro tem o objetivo de garantir uma aprendizagem sólida e significativa, que propicie a formação humana e científica, além da visão crítica voltada para a construção de um mundo mais justo e feliz.</p>
-        <p class="desc">Cada ação é planejada de maneira tal que os adolescentes encontrem na escola o aconchego e acolhimento necessários para o seu pleno desenvolvimento em todas as áreas, preparando-os para os desafios que a vida pode apresentar.</p>
+        <p class="text">Todo o trabalho desenvolvido no Luiza de Teodoro tem o objetivo de garantir uma aprendizagem sólida e significativa, que propicie a formação humana e científica, além da visão crítica voltada para a construção de um mundo mais justo e feliz.</p>
+        <p class="text">Cada ação é planejada de maneira tal que os adolescentes encontrem na escola o aconchego e acolhimento necessários para o seu pleno desenvolvimento em todas as áreas, preparando-os para os desafios que a vida pode apresentar.</p>
         <v-btn class="mt-10 text-button" style="background: #18A830; color: #fff;">garanta a matrícula</v-btn>
       </v-col>
     </v-row>
 
-    <!-- Cursos -->
-    <div class="cursos">
-      <p class="mt-10 mb-10 text-h4 text-center font-weight-bold">Nossos Cursos</p>
+    <!-- Courses -->
+    <div class="courses">
+      <p class="mt-10 mb-10 text-h4 text-center font-weight-bold">Nossos courses</p>
       <v-row>
         <v-col>
           <div class="ma-4" align="center">
@@ -26,13 +26,13 @@
           </div>
         </v-col>
         <v-col>
-          <div class="ma-4" align="center">
+          <div class="ma-4" align-center>
             <v-img class="rounded-xl" width="335" :aspect-ratio="1" src="../assets/curso-logistica.png"></v-img>
             <v-btn class="mt-10 text-button" style="background: #18A830; color: #fff;">Logística</v-btn>
           </div>
         </v-col>
         <v-col>
-          <div class="ma-4" align="center">
+          <div class="ma-4" align-center>
             <v-img class="rounded-xl" width="335" :aspect-ratio="1" src="../assets/curso-enfermagem.png"></v-img>
             <v-btn class="mt-10 text-button" style="background: #18A830; color: #fff;">Enfermagem</v-btn>
           </div>
@@ -49,8 +49,8 @@
       </v-col>
       <v-col class="ma-2">
         <p class="text-h3 mb-8 font-weight-bold">Infraestrutura LTV</p>
-        <p class="desc">Seu filho precisa de espaços inovadores, criativos e seguros para alcançar o melhor desenvolvimento cognitivo, psicossocial e físico. E é essa estrutura que o Luiza oferece a você!</p>
-        <p class="desc">Além de uma equipe multidisciplinar para acolher os alunos, dispomos de ambientes amplos, lúdicos, modernos e interativos para proporcionar o crescimento pleno do estudante.</p>
+        <p class="text">Seu filho precisa de espaços inovadores, criativos e seguros para alcançar o melhor desenvolvimento cognitivo, psicossocial e físico. E é essa estrutura que o Luiza oferece a você!</p>
+        <p class="text">Além de uma equipe multidisciplinar para acolher os alunos, dispomos de ambientes amplos, lúdicos, modernos e interativos para proporcionar o crescimento pleno do estudante.</p>
         <v-btn class="mt-10 text-button" style="background: #18A830; color: #fff;">garanta a matrícula</v-btn>
       </v-col>
     </v-row>
@@ -68,7 +68,7 @@
 </script>
 
 <style scoped>
-  .desc{
+  .text{
     font-size: 20px;
     letter-spacing: 0.7px;
   }
